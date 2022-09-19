79020a43f272e66219a0a7d905767cfc91ee9a44
Sinan <sinanfen@hotmail.com>
2022-09-20 01:39:34 +0300

    Generic repository design pattern completed

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();//Sorguda çalışmak istiyorsak IQueryable- memoryde çalışmasını istersek IEnumerable
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
+    }
+}
